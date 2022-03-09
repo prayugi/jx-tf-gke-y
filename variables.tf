ae4bdd5cef5213476f2653933a02237dfff9912b
prayugi <da.prayugi@gmail.com>
2022-03-09 09:33:36 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "cluster_name" {
 variable "cluster_location" {
   description = "The location (region or zone) in which the cluster master will be created. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region"
   type        = string
-  default     = "us-central1-a"
+  default     = "asia-southeast1-b"
 }
 
 variable "resource_labels" {
